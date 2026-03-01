41ee13ad5bba3af626d1a534af50f3ec4fad39e4
Muhammad Anas <mannas.632006@icloud.com>
2026-03-01 17:13:58 +0500

    Update node.py
    Fixed Coordinate System (Major Fix)
    
    Before:
    
    self.x = row * width
    
    self.y = col * width
    
    This swaps x/y meaning and is confusing.
    
    Now:
    
    self.x = col * width
    
    self.y = row * width

--- node.py
+++ node.py
@@ -1,70 +1,92 @@
 import pygame
 
-# Colors for Visualization
-WHITE = (255, 255, 255)  # Empty
-BLACK = (0, 0, 0)        # Wall
-GREEN = (0, 255, 0)      # Path
-RED = (255, 0, 0)        # Visited
-YELLOW = (255, 255, 0)   # Frontier (Priority Queue)
-ORANGE = (255, 165, 0)   # Start
-PURPLE = (128, 0, 128)   # Goal
-GREY = (128, 128, 128)   # Grid lines
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ORANGE = (255, 165, 0)     # start
+PURPLE = (128, 0, 128)     # goal
+RED = (255, 0, 0)          # closed set
+GREEN = (0, 255, 0)        # path
+BLUE = (0, 0, 255)         # current agent position
+CYAN = (0, 255, 255)       # open set
+GREY = (128, 128, 128)
+
 
 class Node:
     def __init__(self, row, col, width, total_rows):
         self.row = row
         self.col = col
-        self.x = row * width
-        self.y = col * width
-        self.color = WHITE
-        self.neighbors = []
         self.width = width
         self.total_rows = total_rows
+
+        # Standard convention:
+        # x increases to the right (col), y increases downward (row)
+        self.x = col * width
+        self.y = row * width
+
+        self.color = WHITE
+        self.neighbors = []
 
     def get_pos(self):
         return self.row, self.col
 
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == CYAN
+
     def is_wall(self):
         return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == PURPLE
 
     def reset(self):
         self.color = WHITE
 
-    def make_start(self):
-        self.color = ORANGE
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = CYAN
 
     def make_wall(self):
         self.color = BLACK
 
-    def make_goal(self):
+    def make_start(self):
+        self.color = ORANGE
+
+    def make_end(self):
         self.color = PURPLE
 
-    def make_visited(self):
-        if self.color not in [ORANGE, PURPLE]:
-            self.color = RED
-
-    def make_frontier(self):
-        if self.color not in [ORANGE, PURPLE]:
-            self.color = YELLOW
-
     def make_path(self):
-        if self.color not in [ORANGE, PURPLE]:
-            self.color = GREEN
+        self.color = GREEN
+
+    def make_current(self):
+        self.color = BLUE
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
         self.neighbors = []
-        # Check Down, Up, Right, Left
-        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_wall():
-            self.neighbors.append(grid[self.row + 1][self.col])
+        r, c = self.row, self.col
 
-        if self.row > 0 and not grid[self.row - 1][self.col].is_wall():
-            self.neighbors.append(grid[self.row - 1][self.col])
+        # Down
+        if r < self.total_rows - 1 and not grid[r + 1][c].is_wall():
+            self.neighbors.append(grid[r + 1][c])
+        # Up
+        if r > 0 and not grid[r - 1][c].is_wall():
+            self.neighbors.append(grid[r - 1][c])
+        # Right
+        if c < self.total_rows - 1 and not grid[r][c + 1].is_wall():
+            self.neighbors.append(grid[r][c + 1])
+        # Left
+        if c > 0 and not grid[r][c - 1].is_wall():
+            self.neighbors.append(grid[r][c - 1])
 
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_wall():
-            self.neighbors.append(grid[self.row][self.col + 1])
-
-        if self.col > 0 and not grid[self.row][self.col - 1].is_wall():
-            self.neighbors.append(grid[self.row][self.col - 1])
+    def __lt__(self, other):
+        return False
